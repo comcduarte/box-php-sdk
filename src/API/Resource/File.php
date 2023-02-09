8fd1bb1478dc7359e407741cd9559ad242cc8e44
Duartec <Duartec@IT-WKS017.MidNet.CityOfMiddletown.com>
2023-02-09 14:01:43 -0500

    Download File

--- src/API/Resource/File.php
+++ src/API/Resource/File.php
@@ -103,6 +103,63 @@ class File extends AbstractResource implements ArraySerializableInterface
     
     public function delete_file() {}
     
+    /**
+     * Returns the contents of a file in binary format.
+     */
+    public function download_file(string $file_id = null, array $query = null)
+    {
+        if (!isset($file_id)) {
+            return false;
+        }
+        
+        $endpoint = 'https://api.box.com/2.0/files/:file_id/content';
+        
+        $params = [
+            ':file_id' => $file_id,
+        ];
+        
+        $query = ['access_token' => $this->token];
+        
+        if (isset($query)) {
+            $endpoint .= '?:query';
+            $params[':query'] = '';
+            
+            foreach ($query as $field => $value) {
+                $params[':query'] .= sprintf('%s=%s', $field, $value);
+            }
+        }
+        
+        $uri = strtr($endpoint, $params);
+        $this->response = $this->get($uri);
+        
+        switch ($this->response->getStatusCode())
+        {
+            case 200:
+                /**
+                 * 
+                 */
+                return $this->getResponse();
+            case 202:
+                /**
+                 * If the file is not ready to be downloaded yet Retry-After header will be returned indicating the time in seconds after which the file will be available for the client to download.
+                 * This response can occur when the file was uploaded immediately before the download request.
+                 */
+                
+            case 302:
+                /**
+                 * If the file is available to be downloaded the response will include a Location header for the file on dl.boxcloud.com.
+                 * The dl.boxcloud.com URL is not persistent and clients will need to follow the redirect in order to actually download the file.
+                 */
+            default:
+                /**
+                 * An unexpected client error.
+                 */
+                $error = new ClientError();
+                $error->hydrate($this->response);
+                return $error;
+        }
+    }
+    
     public function exchangeArray(array $array)
     {
         foreach (array_keys(get_object_vars($this)) as $var) {
