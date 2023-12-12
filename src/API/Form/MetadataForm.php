1d81872ed7b827cdd2b66da6303a4521f44b114e
Christopher Duarte <christopher.duarte@middletownct.gov>
2023-12-12 15:47:59 -0500

    Removed non-API object reference

--- src/API/Form/MetadataForm.php
+++ src/API/Form/MetadataForm.php
@@ -1,7 +1,6 @@
 <?php
 namespace Laminas\Box\API\Form;
 
-use Application\Model\MetadataTemplateFieldModel;
 use Laminas\Box\API\Resource\MetadataTemplate;
 use Laminas\Form\Form;
 use Laminas\Form\Element\Csrf;
@@ -25,9 +24,6 @@ class MetadataForm extends Form
     {
         $inputFilter = new InputFilter();
         
-        /**
-         * @var MetadataTemplateFieldModel $field
-         */
         foreach ($this->metadata_template->fields as $field) {
             switch ($field->type) {
                 case 'enum':
