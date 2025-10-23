a595bfeacbe7c0effc7476bbd70202b299916349
duartec <duartec@RD-WKS003.midnet.cityofmiddletown.com>
2025-10-23 14:03:08 -0400

    WIP

--- src/API/Resource/BaseResource.php
+++ src/API/Resource/BaseResource.php
@@ -12,4 +12,24 @@ class BaseResource
     public string $id;
 
     public ResourceType $type;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setType($type)
+    {
+        $this->type = ResourceType::from($type);
+    }
 }
